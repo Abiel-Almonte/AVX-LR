e48e2a6b408acf80e49b08d1c073664c277aacb9
Abiel Almonte <almonteabiel@gmail.com>
2025-02-11 23:54:00 -0500

    benchingmarking inference

--- src/main.cpp
+++ src/main.cpp
@@ -1,259 +1,133 @@
-#include <immintrin.h>
-#include <array>
-#include <cmath>
+#include "utils/containers.hh"
+#include "utils/avx.hh"
+#include "utils/tools.hh"
+#include "utils/scalar.hh"
+
 #include <iostream>
-
-constexpr float MAX_Q= 127.9940f;
-constexpr float MIN_Q= -128.0f;
-constexpr float SCALE_FACTOR= 256.0f;
-constexpr float ROUND_FACTOR= 0.5f;
-
-constexpr inline int16_t float_to_q8_8(float n){
-    return static_cast<int16_t>(n* SCALE_FACTOR + ROUND_FACTOR);
-}
-
-constexpr inline float q8_8_to_float(int16_t n){
-    return static_cast<float>(n)/ SCALE_FACTOR; 
-}
-
-constexpr inline float sigmoid(float x){
-    return 1.0f/ (1.0f + std::exp(-x));
-}
-
-constexpr std::array<int16_t, 2048> precompute_sigmoid(){
-    std::array<int16_t, 2048> sigmoidTable{};
-    sigmoidTable[0]= float_to_q8_8(0); 
-    sigmoidTable[2047]= float_to_q8_8(1);
-    for(int i= 1; i < 2047; i++){
-        float input_float= q8_8_to_float(i - 1024);
-        sigmoidTable[i]=  float_to_q8_8(sigmoid(input_float));
-    }
-    return sigmoidTable;
-}
-
-//CXX 20
-alignas(64) constexpr std::array<int16_t, 2048> SIGMOID_TABLE = precompute_sigmoid();
-
-const __m256 MAX_QUANT= _mm256_set1_ps(MAX_Q);
-const __m256 MIN_QUANT= _mm256_set1_ps(MIN_Q);
-const __m256 SCALE= _mm256_set1_ps(SCALE_FACTOR);
-const __m256 ROUND= _mm256_set1_ps(ROUND_FACTOR);
-
-static inline __m256 clamp(__m256 n, __m256 min, __m256 max){
-    return _mm256_max_ps(min, _mm256_min_ps(max, n));
-}
-
-static inline float clamp(float n, float min, float max){
-    return  std::max(min, std::min(max, n));
-}
-
-static inline int16_t clamp(int16_t n, int16_t min, int16_t max){
-    return  std::max(min, std::min(max, n));
-}
-
-int16_t sigmoidApprox_q8_8(int16_t n){
-    int16_t i= clamp(n, -1024, 1023) + 1024;
-    return SIGMOID_TABLE[i];
-}
-
-int16_t sigmoidApprox_q8_8(int32_t n){
-    int16_t n_int16= static_cast<int16_t>(n >>8);
-    int16_t i= clamp(n_int16, -1024, 1023) + 1024;
-    return SIGMOID_TABLE[i];
-}
-
-float sigmoidApprox_float(int16_t n){
-    return q8_8_to_float(sigmoidApprox_q8_8(n));
-}
-
-float sigmoidApprox_float(int32_t n){
-    return q8_8_to_float(sigmoidApprox_q8_8(n));
-}
-
-void quantize8_8_inplace(float* v, int16_t* q, int size){
-    //Do not use size where size%32 != 0. Or feel latency
-    int i= 0;
-    for(; i <= size - 16; i+= 16){
-        _mm_prefetch(reinterpret_cast<const char*>(&v[i+ 64]), _MM_HINT_T0);
-        __m256 vec1_fp= _mm256_load_ps(&v[i]);
-        __m256 vec2_fp= _mm256_load_ps(&v[i + 8]);
-
-        vec1_fp= clamp(vec1_fp, MIN_QUANT, MAX_QUANT);
-        vec2_fp= clamp(vec2_fp, MIN_QUANT, MAX_QUANT);
-        
-        __m256 vec1_fp_scaled= _mm256_fmadd_ps(vec1_fp, SCALE, ROUND);
-        __m256 vec2_fp_scaled= _mm256_fmadd_ps(vec2_fp, SCALE, ROUND);
-
-        __m256i vec1_pi= _mm256_cvtps_epi32(vec1_fp_scaled);
-        __m256i vec2_pi= _mm256_cvtps_epi32(vec2_fp_scaled);
-
-        __m256i vec_pi= _mm256_packs_epi32(vec1_pi, vec2_pi);
-        _mm256_store_si256(reinterpret_cast<__m256i*>(&q[i]), vec_pi);
-    }
-
-    for (; i < size; i++) {
-        float val= v[i];
-        val= clamp(val, MIN_Q, MAX_Q);
-        val= val * SCALE_FACTOR + ROUND_FACTOR;
-        q[i]= static_cast<int16_t>(val);
-    }
-}
-int16_t dotproduct_q8_8(int16_t* w_q8_8, int16_t* x_q8_8, int size){
-    __m256i vec_sum_q16_16 = _mm256_setzero_si256();
-    int i = 0;
-    for(; i <= size - 32; i += 32){ 
-        _mm_prefetch(reinterpret_cast<const char*>(&w_q8_8[i+ 128]), _MM_HINT_T0);
-        _mm_prefetch(reinterpret_cast<const char*>(&x_q8_8[i+ 128]), _MM_HINT_T0);
-
-        __m256i vec1_w_q8_8 = _mm256_load_si256((__m256i*)&w_q8_8[i]);
-        __m256i vec1_x_q8_8 = _mm256_load_si256((__m256i*)&x_q8_8[i]);
-        __m256i dot1= _mm256_madd_epi16(vec1_w_q8_8, vec1_x_q8_8);
-
-        __m256i vec2_w_q8_8 = _mm256_load_si256((__m256i*)&w_q8_8[i+ 16]);
-        __m256i vec2_x_q8_8 = _mm256_load_si256((__m256i*)&x_q8_8[i+ 16]);
-        __m256i dot2= _mm256_madd_epi16(vec2_w_q8_8, vec2_x_q8_8);
-
-        __m256i prod= _mm256_add_epi32(dot1, dot2);
-        vec_sum_q16_16 = _mm256_add_epi32(vec_sum_q16_16, prod);
-    }
-
-    __m128i sum_q16_16_lower= _mm256_castsi256_si128(vec_sum_q16_16);
-    __m128i sum_q16_16_higher= _mm256_extracti128_si256(vec_sum_q16_16, 1);
-    __m128i sum_q16_16_128= _mm_add_epi32(sum_q16_16_lower, sum_q16_16_higher);
-
-    sum_q16_16_128= _mm_hadd_epi32(sum_q16_16_128, sum_q16_16_128);
-    sum_q16_16_128= _mm_hadd_epi32(sum_q16_16_128, sum_q16_16_128);
-    int32_t sum_q16_16= _mm_cvtsi128_si32(sum_q16_16_128);
-
-    for(; i < size; i++){
-        sum_q16_16 += w_q8_8[i] * x_q8_8[i];
-    }
-    return static_cast<int16_t>(sum_q16_16 >> 8);
-}
-
-template <typename T> 
-class alignedVector{
-    private: 
-        size_t size_m;
-        T* ptr_m;
-
-    public:
-        explicit alignedVector(size_t size): size_m(size), ptr_m(nullptr){
-            ptr_m= static_cast<T*>(std::aligned_alloc(32, size*sizeof(T)));
-            if(!ptr_m){
-                throw std::bad_alloc();
-            }
-        }
-        
-        alignedVector(size_t alignment, size_t size): size_m(size), ptr_m(nullptr){
-            ptr_m= static_cast<T*>(std::aligned_alloc(alignment, size*sizeof(T)));
-            if (!ptr_m){
-                throw std::bad_alloc();
-            }
-        }
-
-        alignedVector(): size_m(0), ptr_m(nullptr){};
-
-        alignedVector(const alignedVector&)= delete;
-        alignedVector& operator=(const alignedVector&)= delete;
-
-        alignedVector(alignedVector&& other) noexcept: size_m(other.size_m), ptr_m(other.ptr_m){
-            other.ptr_m= nullptr;
-            other.size_m= 0;
-        }
-
-        alignedVector& operator=(alignedVector&& other) noexcept {
-            if (this != &other){
-                free(ptr_m);
-                ptr_m= other.ptr_m;
-                size_m= other.size_m;
-                other.ptr_m= nullptr;
-                other.size_m= 0;
-            }
-            return *this;
-        }
-
-        ~alignedVector(){
-            free(ptr_m);
-        }
-
-        T* data() const {
-            return ptr_m;
-        }
-        
-        size_t size() const {
-            return size_m;
-        }
-
-        const T& operator [] (size_t index) const {
-            assert(index < size_m && "Index out of bounds");
-            return *(ptr_m + index);
-        }
-
-        T& operator [] (size_t index) {
-            assert(index < size_m && "Index out of bounds");
-            return *(ptr_m + index);
-        }
-
-};
-
-struct AdamWParams{
-    const float beta1;
-    const float beta2;
-    const float beta1_complement;
-    const float beta2_complement;
-    float beta1i;
-    float beta2i;
-    const float decay;
-    const float learning_rate;
-    const float eps;
-
-    alignedVector<float> moment1;
-    alignedVector<float> moment2;
-
-    AdamWParams(size_t size): 
-        learning_rate(1e-3f), 
-        beta1(0.9f),
-        beta2(0.999f),
-        beta1_complement(0.1f),
-        beta2_complement(1e-3f),
-        beta1i(0.9f), 
-        beta2i(0.999f),
-        decay(0.99999f), 
-        eps(1e-8f),
-        moment1(size), 
-        moment2(size)
-    {   
-        __m256 zeros= _mm256_setzero_ps();
-
-        size_t i= 0;
-        for(; i <= size; i+= 8){
-            _mm256_store_ps(moment1.data() + i, zeros);
-            _mm256_store_ps(moment2.data() + i, zeros);
-        }
-
-        for(; i < size; i++){
-            moment1[i]= 0.0f;   
-            moment2[i]= 0.0f;
-        }
-            
-    }
-        
-};
-
 #include <random>
 #include <chrono>
-#include <algorithm>
 
-float dotproduct_scalar_fp(float* weights, float* activations, int size) {
-    float accumulated = 0.0f;
-    #pragma clang loop vectorize(disable)
-    for (int i = 0; i < size; i++) {
-        accumulated += weights[i] * activations[i];
+void analyze_timings(std::vector<double> timings, const char* title){
+     if (timings.empty()) {
+        std::cout << "No data provided for " << title << std::endl;
+        return;
     }
-    return accumulated;
+    
+    std::sort(timings.begin(), timings.end());
+    double median= timings[timings.size()/2];
+    double mean= std::accumulate(timings.begin(), timings.end(), 0.0)/timings.size(); 
+    double p95= timings[static_cast<int>(timings.size()*0.95)];
+    double p99= timings[static_cast<int>(timings.size()*0.99)];
+
+    std::cout << "===== "<< title << " Speed Benchmark =====" << std::endl;
+    std::cout << "Median Latency  (P50): " << median << " ns" << std::endl;
+    std::cout << "Mean Latency    (Avg): " << mean << " ns" << std::endl;
+    std::cout << "95th Percentile (P95): " << p95 << " ns" << std::endl;
+    std::cout << "99th Percentile (P99): " << p99 << " ns" << std::endl;
 }
 
+void analyze_errors(std::vector<double> errors, const char* title) {
+    if (errors.empty()) {
+        std::cout << "No error data provided for " << title << std::endl;
+        return;
+    }
+    
+    std::sort(errors.begin(), errors.end());
+    double median = errors[errors.size() / 2];
+    double mean = std::accumulate(errors.begin(), errors.end(), 0.0) / errors.size();
+    double p95 = errors[static_cast<int>(errors.size() * 0.95)];
+    double p99 = errors[static_cast<int>(errors.size() * 0.99)];
+
+    std::cout << "===== " << title << " Error Benchmark =====" << std::endl;
+    std::cout << "Median Relative Error  (P50): " << median * 100 << " %" << std::endl;
+    std::cout << "Mean Relative Error    (Avg): " << mean * 100 << " %" << std::endl;
+    std::cout << "95th Percentile Relative Error (P95): " << p95 * 100 << " %" << std::endl;
+    std::cout << "99th Percentile Relative Error (P99): " << p99 * 100 << " %" << std::endl;
+}
+
+
+template <size_t feature_size>
+void benchmark_inference(int iterations){
+    alignedArray<int16_t> avx_q8_8_weights(feature_size);
+    alignedArray<int16_t> avx_q8_8_inputs(feature_size);
+
+    alignedArray<float> avx_weights(feature_size);
+    alignedArray<float> avx_inputs(feature_size);
+
+    std::array<float, feature_size> scalar_weights{};
+    std::array<float, feature_size> scalar_inputs{};
+    
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<float> dist_weights(-128, 128);
+    std::uniform_real_distribution<float> dist_inputs(-1, 1);
+
+    for (size_t i = 0; i< feature_size; i++){
+        float rand_w= dist_weights(mt);
+        float rand_x= dist_inputs(mt);
+
+        scalar_weights[i]= rand_w;
+        scalar_inputs[i]= rand_x;
+        
+        avx_weights[i]= rand_w;
+        avx_inputs[i]= rand_x;
+    }
+
+    quantize8_8_inplace(scalar_weights.data(), avx_q8_8_weights.data(), feature_size);
+    quantize8_8_inplace(scalar_inputs.data(), avx_q8_8_inputs.data(), feature_size);
+
+    std::vector<double> avx_q8_8_latency{};
+    std::vector<double> avx_latency{};
+    std::vector<double> scalar_latency{};
+    std::vector<double> relative_errors{};
+    scalar_latency.reserve(iterations);
+    avx_latency.reserve(iterations);
+    avx_q8_8_latency.reserve(iterations);
+
+    volatile float accumulation= 0.0f;
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    std::chrono::duration<double> duration;
+
+    for(int i= 0; i < iterations; i++){ 
+        
+        start= std::chrono::high_resolution_clock::now();
+        float scalar_output= sigmoid_scalar(dotproduct_scalar(scalar_weights.data(), scalar_inputs.data(), feature_size));
+        end= std::chrono::high_resolution_clock::now();
+        duration= end - start;
+        scalar_latency.push_back(duration.count()* 1e9);
+
+        start= std::chrono::high_resolution_clock::now();
+        int16_t avx_output_q8_8= sigmoidApprox_fp_to_q8_8(dotproduct_fp(avx_weights.data(), avx_inputs.data(), feature_size));
+        end= std::chrono::high_resolution_clock::now();
+        duration= end -start;
+        avx_latency.push_back(duration.count()* 1e9);
+        accumulation+= q8_8_to_float(avx_output_q8_8);
+        
+        start= std::chrono::high_resolution_clock::now();
+        int16_t avx_q8_8_output= sigmoidApprox_q8_8(dotproduct_q8_8(avx_q8_8_weights.data(), avx_q8_8_inputs.data(), feature_size));
+        end= std::chrono::high_resolution_clock::now();
+        duration= end -start;
+        avx_q8_8_latency.push_back(duration.count()* 1e9);
+
+        float avx_q8_8_output_fp= q8_8_to_float(avx_q8_8_output);
+
+        double rel_error = (std::fabs(scalar_output) > 1e-6) ? std::fabs(avx_q8_8_output_fp - scalar_output) / std::fabs(scalar_output) : std::fabs(avx_q8_8_output_fp - scalar_output);
+        relative_errors.push_back(rel_error);
+
+        accumulation+= avx_q8_8_output_fp;
+        accumulation+= scalar_output;
+    }
+
+    analyze_timings(scalar_latency, "Scalar Inference");
+    analyze_timings(avx_latency, "AVX Inferece");
+    analyze_timings(avx_q8_8_latency, "AVX Q(8.8) Inference");
+    analyze_errors(relative_errors, "AVX Q(8.8) Error");
+
+}
+
+
 int main() {
+    benchmark_inference<32>(1e9);
+    benchmark_inference<8192>(1e9);
     return 0;
 }
